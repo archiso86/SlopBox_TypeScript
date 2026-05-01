87df70df1547e1442ddbba5f4e40febd29311e81
archiso86 <goober.13@icloud.com>
2026-05-01 18:35:53 -0500

    change theme

--- editor/ColorConfig.ts
+++ editor/ColorConfig.ts
@@ -14,7 +14,7 @@ export interface ChannelColors extends BeepBoxOption {
 export class ColorConfig {
     public static colorLookup: Map<number, ChannelColors> = new Map<number, ChannelColors>();
 	public static usesColorFormula: boolean = false;
-	public static readonly defaultTheme: string = "violet verdant";
+	public static readonly defaultTheme: string = "moonlight";
     public static readonly themes: { [name: string]: string } = {
 	"dark classic": ``,
         "dark competition": `
